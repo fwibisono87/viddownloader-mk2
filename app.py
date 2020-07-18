33cbd218a5b496e286b135712312fa1f345d72bc
Francis Wibisono <fws8787@gmail.com>
2020-07-18 18:45:31 +0700

    fixed input.html

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 from flask import *
 import wtforms
 import youtube
+import os
 
 app = Flask(__name__)
+vidPath = os.path.join(os.getcwd(), 'temp', 'video.mp4')
+audPath = os.path.join(os.getcwd(), 'temp', 'audio.mp4')
+temPath = os.path.join(os.getcwd(), 'temp', 'temp.mp4')
 
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route('/input')
+def add():
+    return render_template("input.html")
 
 
 if __name__ == '__main__':
